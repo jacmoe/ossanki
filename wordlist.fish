1cea4f9acf321ef6c29dcfaf678dc2040571fcc1
jacmoe <jacmoe.dk@gmail.com>
2023-02-21 15:00:43 +0100

    Script to process images and generate matching ipa

--- wordlist.fish
+++ wordlist.fish
@@ -1,2 +1,23 @@
 #! /usr/bin/fish
-ls words | awk '!/^$/' | sed 's/.png//' | xargs -I {} dict -d gcide {} > words_processed.txt
+ls words2 | awk '!/^$/' | sed 's/.png//' > words2.txt
+
+awk '
+    FNR == NR {
+      # reading file1
+      words[$1] = ""
+      next
+    }
+    {
+      # reading file2
+      for (elem in words)
+        if (tolower($2) == elem)
+          if (values[elem] == "")
+            values[elem] = substr($3, 2)
+          else
+            values[elem] = values[elem] substr($3, 2)
+    }
+    END {
+      for (elem in values)
+        print elem, values[elem]
+    }
+' ./words2.txt ~/projects/cmudict-ipa/brown-frequency-list-with-ipa.txt | tee words2_processed.txt
