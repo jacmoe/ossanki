128de0b0bd8a552cc36f0eeafe5a1045e38f66fd
jacmoe <jacmoe.dk@gmail.com>
2023-04-10 02:08:27 +0200

    cmudict-ipa is in 'sources'

--- ipa_local.sh
+++ ipa_local.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 # Script uses IPA database with 125,000 words, downloaded from https://github.com/menelik3/cmudict-ipa
-awk -v w=$1 ' w == tolower($1) {print $2} ' ~/projects/cmudict-ipa/cmudict-0.7b-ipa.txt |\
+awk -v w=$1 ' w == tolower($1) {print $2} ' ~/sources/cmudict-ipa/cmudict-0.7b-ipa.txt |\
 xargs -I {} echo $1 {} | tee result.txt
